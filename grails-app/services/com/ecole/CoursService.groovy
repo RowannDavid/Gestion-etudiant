cdab37fdfa6062378d5691dfecd20dee2f236882
👑Lord_Clxt👑 <127218888+LordClxt@users.noreply.github.com>
2026-06-15 12:03:59 +0000

    Enhance CoursService with logging and error handling
    Refactor CoursService to improve error handling and logging.

--- grails-app/services/com/ecole/CoursService.groovy
+++ grails-app/services/com/ecole/CoursService.groovy
@@ -6,26 +6,32 @@ import grails.gorm.transactions.Transactional
 
 @Transactional
 class CoursService {
-
+    // Evite le nommage en français
     CoursResponseDTO creerCours (CoursRequestDTO dto) {
+        // Ajoute du logging check doc(Slf4j)
+        // Evite le typage dynamique
         def cours = new Cours(
                 titre: dto.titre,
                 description: dto.description,
                 dureeMinutes: dto.dureeMinutes,
                 place: dto.place
         )
-
+        
+        // Utilise failOnError tu auras plus de detail sur ce qui à déclanché l'erreur de la sauvegarde car ton message ne dit pas ce qui a foiré la sauvegarde
         if (!cours.save(flush: true)) {
             throw new RuntimeException(
                     "impossible de creer un cours"
             )
         }
 
+        
         return toResponseDTO(cours)
     }
-
+    
+    // Evite le nommage en français
     @Transactional(readOnly = true)
     List<CoursResponseDTO> listeCours() {
+        // Ajoute du logging check doc(Slf4j)
         def listecours = Cours.findAll()
         return listecours.collect { cours ->
             toResponseDTO(cours)
@@ -34,6 +40,8 @@ class CoursService {
 
     @Transactional(readOnly = true)
     CoursResponseDTO detailCours(Long id) {
+        // Ajoute du logging check doc(Slf4j)
+        // Crée une methode getById pour eviter la répétition de code
         def cours = Cours.get(id)
         if (!cours) {
             throw new RuntimeException(
@@ -43,7 +51,10 @@ class CoursService {
         return toResponseDTO(cours)
     }
 
+    // Evite le nommage en français
     CoursResponseDTO modifierCours (CoursRequestDTO dto, Long id) {
+        // Ajoute du logging check doc(Slf4j)
+        // Crée une methode getById pour eviter la répétition de code
         def cours = Cours.get(id)
         if (!cours) {
             throw new RuntimeException(
@@ -65,8 +76,10 @@ class CoursService {
         return toResponseDTO(cours)
     }
 
-
+    // Evite le nommage en français
     void supprimerCours(Long id) {
+        // Ajoute du logging check doc(Slf4j)
+        // Crée une methode getById pour eviter la répétition de code
         def cours = Cours.get(id)
         if (!cours) {
             throw new RuntimeException(
@@ -76,7 +89,10 @@ class CoursService {
         cours.delete(flush: true)
     }
 
+    // Evite le nommage en français
+    // Crée un constrcuteur dans ton dto qui prends le domain cours en paramètre et retourne directement ton dto(Dit moi si tu ne comprends pas)
     private CoursResponseDTO toResponseDTO(Cours cours) {
+        // Ajoute du logging check doc(Slf4j)
         new CoursResponseDTO(
                 id: cours.id,
                 titre: cours.titre,
